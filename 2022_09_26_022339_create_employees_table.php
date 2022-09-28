b1df865970be1aa4ea1fb771d2e170721252ac24
06hoang05 <97800448+06hoang05@users.noreply.github.com>
2022-09-28 09:02:16 +0700

    Add files via upload

---
+++ 2022_09_26_022339_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('employee_name');
+            $table->string('employee_addrs');
+            $table->integer('employee_phone');
+            $table->string('employee_email');
+            $table->integer('employee_salay');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
